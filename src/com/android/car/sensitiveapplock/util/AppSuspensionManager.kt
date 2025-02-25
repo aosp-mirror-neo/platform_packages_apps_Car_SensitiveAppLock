39c3e85e6811a123d83cd8126ca9ea3c035a5e8c
Swapnil Patel <swpatel@google.com>
2025-02-24 16:00:16 -0800

    Create AppSuspensionManager to manage app suspension.
    Bug: 398950735
    Change-Id: I71711ed94e9aefba78fa772d6e9c4559994075f5
    Test: atest --host  SensitiveAppLockRoboTests:AppSuspensionManagerTest

---
+++ src/com/android/car/sensitiveapplock/util/AppSuspensionManager.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.car.sensitiveapplock.util
+
+import android.content.Context
+import android.content.pm.SuspendDialogInfo
+import android.content.pm.SuspendDialogInfo.BUTTON_ACTION_MORE_DETAILS
+import com.android.car.sensitiveapplock.R
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/** A manager for controlling app suspension state. */
+@Singleton
+class AppSuspensionManager
+@Inject
+internal constructor(@ApplicationContext applicationContext: Context) {
+    private val packageManager = applicationContext.packageManager
+
+    /**
+     * Sets the suspension state of an app.
+     *
+     * Note: The caller must have the `SUSPEND_APPS` permission.
+     */
+    fun setAppSuspensionState(packageName: String, state: Boolean) {
+        setAppSuspensionState(arrayOf(packageName), state)
+    }
+
+    /**
+     * Sets the suspension state of a list of apps.
+     *
+     * Note: The caller must have the `SUSPEND_APPS` permission.
+     */
+    fun setAppSuspensionState(packageNames: Array<String>, state: Boolean) {
+        logger.d("Suspending ${packageNames.contentToString()} with state: $state")
+
+        val suspendDialogInfo =
+            SuspendDialogInfo.Builder()
+                .setTitle(R.string.suspend_dialog_title)
+                .setMessage(R.string.suspend_dialog_message)
+                .setNeutralButtonText(R.string.suspend_dialog_neutral_button_text)
+                .setNeutralButtonAction(BUTTON_ACTION_MORE_DETAILS)
+                .build()
+
+        packageManager
+            .setPackagesSuspended(
+                packageNames,
+                state,
+                null, // appExtras
+                null, // launcherExtras
+                suspendDialogInfo,
+            )
+            .also { errorPackages ->
+                logger.e("Failed to suspend: ${errorPackages.contentToString()}")
+            }
+    }
+
+    private companion object {
+        val logger = Logger(AppSuspensionManager.javaClass)
+    }
+}
