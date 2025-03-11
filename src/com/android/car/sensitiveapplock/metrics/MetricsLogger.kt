dbcc1ced791cc285e8cc42e5069b29f2bfeef023
Vaibhav Goyal <vagoyal@google.com>
2025-03-10 19:33:32 -0700

    Create a metrics logger for uploading metrics
    Test: Manual testing
    Bug: 398940750
    Change-Id: I1524478210ee8d4f263c6dabd63d7107ed7ecfbd

---
+++ src/com/android/car/sensitiveapplock/metrics/MetricsLogger.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.car.sensitiveapplock.metrics
+
+import android.os.Process
+import android.os.UserHandle
+import com.android.car.sensitiveapplock.data.AppLockDataRepository
+import com.android.car.sensitiveapplock.data.LockableAppsListRepository
+import com.android.car.sensitiveapplock.di.qualifiers.BackgroundContext
+import com.android.internal.widget.LockPatternUtils
+import javax.inject.Inject
+import javax.inject.Singleton
+import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.withContext
+
+/** Helper class for logging metric events. */
+@Singleton
+class MetricsLogger
+@Inject
+constructor(
+    private val appLockDataRepository: AppLockDataRepository,
+    private val lockableAppsListRepository: LockableAppsListRepository,
+    private val lockPatternUtils: LockPatternUtils,
+    @BackgroundContext private val backgroundContext: CoroutineContext
+) {
+    /** Logs the user's current app lock state. */
+    suspend fun logState() = withContext(backgroundContext) {
+        val lockedApps = appLockDataRepository.getLockedApps().toSet()
+        val packageUids = lockableAppsListRepository.getLockableApps().filter {
+            lockedApps.contains(it.packageName)
+        }.map { it.packageUid }
+        val pinSet = appLockDataRepository.getPin().isNotEmpty()
+        val profileLockSet = lockPatternUtils.isSecure(UserHandle.getUserId(Process.myUid()))
+        SensitiveAppLockStatsLog.write(
+            SensitiveAppLockStatsLog.SENSITIVE_APP_LOCK_STATE_CHANGED,
+            pinSet,
+            packageUids.toIntArray(),
+            profileLockSet
+        )
+    }
+}
